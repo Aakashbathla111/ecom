618b9ca5eb309e5a19031d6c91ccf590d022fc6d
Aakashbathla111 <143501806+Aakashbathla111@users.noreply.github.com>
2024-07-07 13:16:36 +0530

    Add files via upload

---
+++ src/firebaseinit.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPP9VRo0deGilda2ziS1qG92zWZktpg70",
+  authDomain: "blogdemo-99171.firebaseapp.com",
+  projectId: "blogdemo-99171",
+  storageBucket: "blogdemo-99171.appspot.com",
+  messagingSenderId: "461378926924",
+  appId: "1:461378926924:web:833d408de9188c7aa877e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
